1cf8fd8e1069f01d3f435a7c66334bad4b38c76d
sgavanka <sgavanka@purdue.edu>
2018-10-02 11:08:02 -0400

    added naviagation drawer

--- app/src/main/java/app/rsez/MainActivity.java
+++ app/src/main/java/app/rsez/MainActivity.java
@@ -1,7 +1,10 @@
 package app.rsez;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public  class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public  class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonLogin = (Button) findViewById(R.id.loginButton);
+
+        // Capture button clicks
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                       HomeActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
