922ba388d55413aacacfaae35eeac868f8064fac
sgavanka <sgavanka@purdue.edu>
2018-10-02 13:59:02 -0400

    fixed gradle scripts

--- app/src/main/java/app/rsez/models/ModelBase.java
+++ app/src/main/java/app/rsez/models/ModelBase.java
@@ -2,6 +2,7 @@ package app.rsez.models;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+
 public class ModelBase {
     protected static FirebaseFirestore db = FirebaseFirestore.getInstance();
 }
