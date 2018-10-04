4ed9e91e4d85d285755da8ff2d4a44bc58ca1f49
Mitch <m.palin@yahoo.com>
2018-10-04 11:59:45 -0400

    Removed unused method convertMonth and unused import statements

--- app/src/main/java/app/rsez/EventDetailsFragment.java
+++ app/src/main/java/app/rsez/EventDetailsFragment.java
@@ -11,12 +11,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.Timestamp;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
-
-import java.util.Date;
-
 import app.rsez.models.Event;
 
 public class EventDetailsFragment extends Fragment {
@@ -30,7 +25,6 @@ public class EventDetailsFragment extends Fragment {
         frag.setArguments(bundle);
         return frag;
     }
-    private FirebaseAuth mAuth;
     static String title;
     static String desc;
     static String date;
@@ -49,7 +43,6 @@ public class EventDetailsFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_event_details, container, false);
-        mAuth = FirebaseAuth.getInstance();
         String eventID = this.getArguments().getString("id");
         Event event = new Event(eventID);
         eventName = (TextView) view.findViewById(R.id.event_name_view);
@@ -78,33 +71,5 @@ public class EventDetailsFragment extends Fragment {
         });
 
         return view;
-    }
-
-    public String convertMonth(int month){
-        if (month == 1){
-            return "Jan";
-        } else if (month == 2){
-            return "Feb";
-        } else if (month == 3){
-            return "Mar";
-        } else if (month == 4){
-            return "Apr";
-        } else if (month == 5){
-            return "May";
-        } else if (month == 6){
-            return "Jun";
-        } else if (month == 7){
-            return "Jul";
-        } else if (month == 8){
-            return "Aug";
-        } else if (month == 9){
-            return "Sep";
-        } else if (month == 10){
-            return "Oct";
-        } else if (month == 11){
-            return "Nov";
-        } else {
-            return "Dec";
-        }
     }
 }
