47b73daf90a984496ebb6915c60f257a10f39c52
Mitch <m.palin@yahoo.com>
2018-10-05 10:22:33 -0400

    Added QRScanner import

--- app/src/main/java/app/rsez/QRScanFragment.java
+++ app/src/main/java/app/rsez/QRScanFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.zxing.Result;
-
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class QRScanFragment extends AppCompatActivity implements ZXingScannerView.ResultHandler {
