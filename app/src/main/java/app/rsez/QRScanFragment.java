1600ff74535a9e1ca410a1a25508fa4dbc8eef59
Mitch <m.palin@yahoo.com>
2018-10-14 14:27:55 -0400

    Added permission request for camera in qrscan function

--- app/src/main/java/app/rsez/QRScanFragment.java
+++ app/src/main/java/app/rsez/QRScanFragment.java
@@ -1,8 +1,10 @@
 package app.rsez;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -14,7 +16,8 @@ public class QRScanFragment extends AppCompatActivity implements ZXingScannerVie
 
 
     private ZXingScannerView zXingScannerView;
-
+    final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    int cameraEnabled = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +28,30 @@ public class QRScanFragment extends AppCompatActivity implements ZXingScannerVie
     public void qrScan(){
         zXingScannerView = new ZXingScannerView(getApplicationContext());
         setContentView(zXingScannerView);
-        zXingScannerView.setResultHandler(this);
-        zXingScannerView.startCamera();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            cameraEnabled = 1;
+        }
+        if (cameraEnabled == 1) {
+            zXingScannerView.setResultHandler(this);
+            zXingScannerView.startCamera();
+        }
     }
 
     @Override
@@ -42,4 +67,27 @@ public class QRScanFragment extends AppCompatActivity implements ZXingScannerVie
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    zXingScannerView.setResultHandler(this);
+                    zXingScannerView.startCamera();
+                    cameraEnabled = 1;
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    cameraEnabled = 0;
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
 }
